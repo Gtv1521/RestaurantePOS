218da2d7d6500d80c1a5dea05aa1c3e9ba64ca9b
Duvan Bernal <user@MacBook-Pro-de-Duvan.local>
2026-08-04 15:04:33 -0500

    feat: Gestion de ingredientes inicial

--- obj/Debug/net8.0/MiComanderaApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MiComanderaApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiComanderaApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85745fb320237a47b6d11e2556ae1a73055d62f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5068a814c6678fd31310fc5dc42ea2cc9b95d89")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiComanderaApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiComanderaApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
